da6ddfa084506eaa977bfd42e571d80535c89c4a
jaume.tarradasllort <jaume.tarradasllort@WKSTN043.3vts.ie>
2013-12-10 20:24:08 +0000

    - created generators as a services - created a separated test controller - removed acme...maybe other day

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,10 +17,10 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Bic\FormValidationBundle\BicFormValidationBundle(),
+            new Bic\FormValidationTestBundle\BicFormValidationTestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
---
+++ src/Bic/FormValidationTestBundle/BicFormValidationTestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bic\FormValidationTestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BicFormValidationTestBundle extends Bundle
+{
+}
